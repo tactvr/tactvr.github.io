0d79515eb52e8a23d37af7e573d9ed065fb92f76
Brad Clarke <john.clarke@hexagonsi.com>
2018-03-20 13:48:21 -0500

    add basic structure of table

--- schedule.js
+++ schedule.js
@@ -1,3 +1,22 @@
 function generateSchedule(containerLocator, scheduleArray) {
-    
+    var jqContainer = $(containerLocator).empty();
+    $.each(scheduleArray, function(key, value){ 
+        $(containerLocator).append(makeRow(value));
+    });
+}
+function makeRow(rowData){
+    var row = $("<div>", { class: "pure-g eventRow"});
+    row.append( $("<div>", {
+        class: "pure-u-1-3",
+        text: rowData.date,
+    }));
+    row.append( $("<div>", {
+        class: "pure-u-1-3",
+        text: rowData.name,
+    }));
+    row.append( $("<div>", {
+        class: "pure-u-1-3",
+        text: "Register",
+    }));        
+    return row;
 }
